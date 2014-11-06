b14286497c742bb1a5e833dda2095a0649ce78bf
BasilMason <basilmason@hotmail.com>
2014-11-06 22:40:00 +0000

    Fraction Add and Add Test added.

---
+++ src/FractionCalculatorTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Basil on 06/11/2014.
+ */
+public class FractionCalculatorTest {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ src/FractionTest.java
@@ -0,0 +1,34 @@
+/**
+ * Created by keith for the second coursework assignment.
+ */
+public class FractionTest {
+    public static void main(String[] args) {
+
+        // test divide by zero - should print an error and exit
+        new Fraction(1, 0);
+        // test multiply
+	Fraction f = new Fraction(3,10);
+	Fraction g = new Fraction(1,2);
+	Fraction h = new Fraction(3,5);
+	if (!f.equals(g.multiply(h))) System.out.println("Multiply failed");
+        // test equals
+	test(new Fraction(1, 2),new Fraction(1, 2),"error test 1");
+	test(new Fraction(1, 2),new Fraction(3, 6),"error test 2");
+	test(new Fraction(-1, 2),new Fraction(1, -2),"error test 3");
+	test(new Fraction(-1, -2),new Fraction(1, 2),"error test 4");
+	test(new Fraction(4, -8),new Fraction(1, 2),"error test 5");
+
+        // extend with extra tests
+		// test addition
+	Fraction i = new Fraction(19,14);
+	Fraction j = new Fraction(2,4);
+	Fraction k = new Fraction(6,7);
+	if (!i.equals(j.add(k))) System.out.println("Addition failed");
+
+    }
+
+    static void test(Fraction f1, Fraction f2, String msg){
+    	if (! f1.equals(f2))
+		System.out.println(msg);
+    }
+}
