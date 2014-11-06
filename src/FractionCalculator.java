6d850d9039848d3c831796bed8fe7d84bc8407f6
BasilMason <basilmason@hotmail.com>
2014-11-06 21:54:47 +0000

    Project Created

---
+++ src/FractionCalculator.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Basil on 06/11/2014.
+ */
+public class FractionCalculator {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+    }
+
+}
