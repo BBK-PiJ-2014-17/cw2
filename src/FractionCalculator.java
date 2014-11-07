64cdac15202bf1333d10e5ce6676a8f9b057d05d
BasilMason <basilmason@hotmail.com>
2014-11-07 00:34:00 +0000

    Initial framework for FractionCalculator class.

--- src/FractionCalculator.java
+++ src/FractionCalculator.java
@@ -1,12 +1,42 @@
 /**
  * Created by Basil on 06/11/2014.
+ * FractionCalculator object
  */
 public class FractionCalculator {
+    private Fraction value;
+    private char operation;
 
-    public static void main(String[] args) {
+    public FractionCalculator() {
+        this.value = new Fraction(0,1); //  initialise calculator value to zero
+        this.operation = '0';   // nil operation
+    }
 
-        System.out.println("Hello, World!");
+    @Override
+    public String toString() {
 
+        String str = (this.operation == '0') ? "nil" : "" + this.operation;   //  print nil operation
+
+        // represent FractionCalculator state by printing current value and saved operation
+        return "Calculator value: " + this.value.toString()
+                + ", operation: " + str;
+    }
+
+    // getter and setters
+
+    public Fraction getValue() {
+        return this.value;
+    }
+
+    private void setValue(Fraction f) {
+        this.value = f;
+    }
+
+    public char getOperation() {
+        return this.operation;
+    }
+
+    private void setOperation(char op) {
+        this.operation = op;
     }
 
 }
--- src/FractionCalculatorTest.java
+++ src/FractionCalculatorTest.java
@@ -1,11 +1,16 @@
 /**
  * Created by Basil on 06/11/2014.
+ * FractionCalculator Tester class
  */
 public class FractionCalculatorTest {
 
     public static void main(String[] args) {
 
+        // create fraction calculator object
 
+        FractionCalculator fc = new FractionCalculator();
+
+        System.out.println(fc.toString());
 
     }
 
